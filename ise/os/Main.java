fe2f4997c8dc637e493e31ffea6b5d3bcb716f71
Fu <1123237008@qq.com>
2021-07-16 14:23:27 +0800

    加了一行注释

--- ise/os/Main.java
+++ ise/os/Main.java
@@ -11,7 +11,7 @@ public class Main {
     
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
-            
+            //一行新的测试代码
             @Override
             public void run() {
                 new MainFrame().setVisible(true);
